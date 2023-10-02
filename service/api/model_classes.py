7b51f208a9ce1998deaf907ed04f3ecaa4fc1506
yliasolom <ylia.solomennikowa@yandex.ru>
2023-10-02 00:36:26 +0000

    add model classes

---
+++ service/api/model_classes.py
@@ -0,0 +1,52 @@
+import dill
+import pandas as pd
+import yaml
+from typing import Optional
+
+with open('service/config/config.yaml') as f:
+    config = yaml.safe_load(f)
+
+
+class UserKNN:
+    model_loaded: bool = False
+    model: Optional[object] = None
+    recs: Optional[pd.DataFrame] = None
+
+    @classmethod
+    def load_model(cls):
+        if not cls.model_loaded:
+            cls.model_loaded = True
+            if config['userknn_model']['online_mode']:
+                with open(config['userknn_model']['online'], 'rb') as f:
+                    cls.model = dill.load(f)
+            else:
+                cls.recs = pd.read_csv(
+                    config['userknn_model']['offline'],
+                    encoding='utf-8',
+                )
+
+class LightFM:
+    model_loaded: bool = False
+    model: Optional[object] = None
+
+    @classmethod
+    def load_model(cls):
+        if not cls.model_loaded:
+            cls.model_loaded = True
+            with open(config['lightfm_model']['online'], 'rb') as f:
+                cls.model = dill.load(f)
+
+
+class Popular:
+    model_loaded: bool = False
+    model: Optional[object] = None
+    recs: Optional[pd.DataFrame] = None
+
+    @classmethod
+    def load_model(cls):
+        if not cls.model_loaded:
+            cls.model_loaded = True
+            cls.recs = pd.read_csv(
+                config['popular_model']['offline'],
+                encoding='utf-8',
+            )
